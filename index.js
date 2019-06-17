0ecfafaedd4f73fde83fc439982969027f5065d5
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-17 22:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function johnLennonFacts(jlfacts) {
    "He was the last Beatle to learn to drive",
    "He was never a vegetarian",
    "He was a choir boy and boy scout",
-   "He hated the sound of his own voice"
+   "He hated the sound of his own voice",
+   "foo",
+   "bar"
      ];
   var newfacts = [];
   var i = 0;

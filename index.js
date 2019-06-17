11b7c461880aef8e33c7b09faa46242ec65e0a2c
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-17 22:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+   let arrayx = [];
+    for (let i=0; i < musicians.length; i++ ) {
+      arrayx.push( musicians[i] + " plays " + instruments[i]);
+      
+  }
+    return arrayx;
+}
+
+function johnLennonFacts(jlfacts) {
+  var facts = [
+    "foo",
+    "bar",
+   
+  ];
+  var newfacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newfacts.push(facts[i] +"!!!");
+    i++;
+  }
+    return newfacts;
+}
